26592bb5ab5b8bb1e8b7d496601f029544f8c8d7
DavOlufuwa <davolufuwa@gmail.com>
2023-02-17 10:40:17 +0100

    added newsletter data

--- src/components/Doctor/Homepage/Homepage.jsx
+++ src/components/Doctor/Homepage/Homepage.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import './Homepage.scss'
+import Hello from '../images/hello.svg' 
 
 const Homepage = () => {
   return (
-    <div>This is the Homepage</div>
+    <div className="main-coat">
+      <header>
+        <div className="main-text">
+          Welcome 
+          <span><img src={Hello} alt="" /></span>
+          Dr Jennifer Jaiye
+        </div>
+      </header>
+      <section>
+        <div className="main-text">
+          Newsletters
+        </div>
+        <div className="content">
+          
+        </div>
+      </section>
+      <section>
+        <div className="main-text"></div>
+        <div className="content"></div>
+      </section>
+      <section>
+        <div className="main-text"></div>
+        <div className="content"></div>
+      </section>
+    </div>
   )
 }
 
---
+++ src/components/Doctor/data/Newsletter.js
@@ -0,0 +1,26 @@
+export const newsLetter = [
+    {
+        id: 1,
+        source: require('../images/image_1.png')
+    },
+    {
+        id: 2,
+        source: require('../images/image_2.png')
+    },
+    {
+        id: 3,
+        source: require('../images/image_3.png')
+    },
+    {
+        id: 4,
+        source: require('../images/image_4.png')
+    },
+    {
+        id: 5,
+        source: require('../images/image_5.png')
+    },
+    {
+        id: 6,
+        source: require('../images/image_6.png')
+    }    
+]
