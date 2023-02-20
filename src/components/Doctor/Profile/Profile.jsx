0863920e22d1c76985186e69f036a679bc6007f5
DavOlufuwa <davolufuwa@gmail.com>
2023-02-21 00:28:54 +0100

    added a profile form

--- src/components/Doctor/Profile/Profile.jsx
+++ src/components/Doctor/Profile/Profile.jsx
@@ -1,8 +1,167 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { profileData } from '../data/Profiledata'
+import { countryList } from '../../countryList'
+
+import './Profile.scss'
+import { Icon } from '@iconify/react'
 
 const Profile = () => {
+  const [profileFormData, setProfileFormData] = useState(profileData) 
+  const [selectedCountry, setSelectedCountry] = useState(profileData.country);
+  const updateFormData = (event)=>{
+    const {name, value} = event.target;
+    setProfileFormData({...profileFormData, [name]: value})
+  }
+
+  const updateForm = (event)=>{
+    event.preventDefault();
+    console.log(profileFormData)
+  }
+
+  const updateCountry = (event)=>{
+    setSelectedCountry(event.target.value)
+    updateFormData()
+  }
+
+
   return (
-    <div>This is the profile page</div>
+    <div>
+      <div className="title">Profile</div>
+      <form className='sign-up-form'>
+        <div>
+          <img src={profileFormData.profilePic} alt="" />
+          <Icon 
+             icon="material-symbols:add-circle-rounded" color="#2ebbbc"
+             className='plus'
+          />
+        </div>
+        <div>
+          <label htmlFor="bio">Bio</label>
+          <textarea  
+            name="bio" 
+            id="bio"
+            value={profileFormData.bio}  
+            onChange={updateFormData}
+            required
+          >
+          </textarea>
+        </div>
+        <div>
+          <label htmlFor="firstname">First name</label>
+          <input 
+            type="text" 
+            name="firstName" 
+            id="firstname" 
+            value={profileFormData.firstName}
+            onChange={updateFormData}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="lastName">LastName</label>
+          <input 
+            type="text" 
+            name="lastName" 
+            id="lastName" 
+            value={profileFormData.lastName}
+            onChange={updateFormData}
+            required  
+          />
+        </div>
+        <div>
+          <label htmlFor="dateOfBirth">Date of Birth</label>
+          <input 
+            type="date"
+            name="dateOfBirth" 
+            id="dateOfBirth" 
+            value={profileFormData.dateOfBirth}
+            onChange={updateFormData}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="sex">Sex</label>
+          <input 
+            type="text" 
+            name="sex" 
+            id="sex" 
+            value={profileFormData.sex}
+            onChange={updateFormData}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="country">Country</label>
+          <select 
+            name="country" 
+            id="country" 
+            value={selectedCountry} 
+            onChange={updateCountry} 
+            required>
+            {
+              countryList.map(country =>(
+                <option key={country} value={country}>
+                  {country}
+                </option>
+              ))
+            }
+          </select>
+        </div>
+        <div>
+          <label htmlFor="phoneNumber">Phone number</label>
+          <input 
+            type="tel" 
+            name="phoneNumber" 
+            id="phoneNumber" 
+            value={profileFormData.phoneNumber}
+            onChange={updateFormData}
+            required/>
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input 
+            type="email"
+            name="email" 
+            id="email" 
+            value={profileFormData.email}
+            onChange={updateFormData}
+            required/>
+        </div>
+        <div>
+          <label htmlFor="specialty">Specialty</label>
+          <input 
+            type="text" 
+            name="specialty" 
+            id="specialty"
+            value={profileFormData.specialty}
+            onChange={updateFormData} 
+            required/>
+        </div>
+        <div>
+          <label htmlFor="yearsOfExperience">Years of experience</label>
+          <input 
+            type="text" 
+            name="yearsOfExperience" 
+            id="yearsOfExperience" 
+            value={profileFormData.yearsOfExperience}
+            onChange={updateFormData}
+            required/>
+        </div>
+        <div>
+          <label htmlFor="workHours">Work hours</label>
+          <input 
+            type="text" 
+            name="workHours" 
+            id="workHours"
+            value={profileFormData.workHours}
+            onChange={updateFormData} 
+            required/>
+        </div>
+        <div>
+          <button className="btn" onClick={updateForm}>Update</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
---
+++ src/components/Doctor/data/Profiledata.js
@@ -0,0 +1,15 @@
+export let profileData = {
+    profilePic: require('../images/profile_photo.png'),
+    bio: 
+        'Dr. Jenifer is reproductive endocrinologist who has additional training in    infertility and fertility treatment. She treats both male and female fertility issues, unlike andrologists.',
+    firstName: 'Jennifer',
+    lastName: 'Jaiye',
+    dateOfBirth: '1980-02-11',
+    sex: 'Female',
+    country: 'Nigeria',
+    phoneNumber: '09033445566',
+    email: 'jeniferjaye@gmail.com',
+    specialty: 'Reproductive endocrinologist',
+    yearsOfExperience: '10 years',
+    workHours: 'Mon - Fri (9:00 AM - 4:00 pm)'
+  };
